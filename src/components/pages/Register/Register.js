afae379fff004d94a3309640832ca04614393385
kairayary <kaira.abreu@gmail.com>
2022-03-02 22:16:22 -0300

    creo funcion d los alert y else if en displayMess

--- src/components/pages/Register/Register.js
+++ src/components/pages/Register/Register.js
@@ -1,25 +1,59 @@
 import React from "react";
 import "../Register/Register.css";
-import {Link as Linkrouter} from "react-router-dom";
+import { Link as Linkrouter } from "react-router-dom";
+import axios from "axios";
 
-function Register (){
+function Register() {
+
+  async function NewUser(event) {
+    event.preventDefault()
+    const NuevoUsuario = {
+      firstname: event.target[0].value,
+      lastname: event.target[1].value,
+      email: event.target[2].value,
+      password: event.target[3].value
+    }
+    console.log(NuevoUsuario)
+    console.log(event)
+
+
+       await axios.post("http://localhost:4000/api/signup",{NuevoUsuario})
+      .then(response => //alert(response.data.response))
+           //  if (response.success === "falseVAL"){
+          // console.log(response.data)
+           // }
+          displayMessages(response.data) //para validar los datos
+
+         )
+      function displayMessages(data){
+         if(data.success==="falseVAL"){
+           console.log(data)
+          console.log(data.response.error.details)
+         alert(data.response.error.details.map(error=>error.message))
+         }else if(data.success==="trueUE"){
+           console.log(data)
+         }
+
+      }
+     }
+
     return (
-        <div className="containerRegiter">
+      <div className="containerRegiter">
         <div className="register">
           <span className="registerTitle">Register</span>
-          <form className="registerForm">
-            <label>User Name</label>
+          <form className="registerForm" onSubmit={NewUser}>
+            <label>Firstname</label>
             <input
               className="regiterInput"
               type="text"
-              placeholder="Enter your username..."
-            /><label>Email</label>
+              placeholder="Enter your firstname..."
+            /><label>LastName</label>
             <input
               className="regiterInput"
               type="text"
-              placeholder="Enter your email..."
+              placeholder="Enter your last name..."
             />
-            <label> Confirm your Email</label>
+            <label>Email</label>
             <input
               className="regiterInput"
               type="text"
@@ -37,13 +71,13 @@ function Register (){
               type="password"
               placeholder="Enter your password..."
             />
-            <button className="registerButton">Register</button>
+            <button type="submit" className="registerButton">Register</button>
           </form>
-        <Linkrouter to="/login">
-          <button className="registerLoginButton">Login</button>
-        </Linkrouter>
+          <Linkrouter to="/login">
+            <button type="submit" className="registerLoginButton">Login</button>
+          </Linkrouter>
         </div>
       </div>
     )
-}
-export default Register;
+  }
+  export default Register;
