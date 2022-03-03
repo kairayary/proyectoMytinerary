c814d8e9dc170b341001dc063ed76d9e7c2df333
kairayary <kaira.abreu@gmail.com>
2022-03-02 22:11:19 -0300

    ruta del signup y de la verificacion del email

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,12 +1,21 @@
 const Router = require("express").Router();
 const datosController = require ("../controllers/datosControllers")// revisar esta ruta
 const{ObtenerDatosCompletos, ObtenerItinerarios}= datosController// extrae la constante obtenerDatosCompletos destructuracion
+const usersControllers =require("../controllers/usersControllers")
+const{nuevoUsuario, verifyEmail} = usersControllers
+const validator = require("../controllers/validator")
+
 
 Router.route("/datos")// datos es parte de la url de la consulta
 .get(ObtenerDatosCompletos)
  Router.route("/itinerary/:city")
  .get(ObtenerItinerarios)
 
+Router.route("/signup")
+.post(validator,nuevoUsuario)
+
+Router.route("/verify/:uniqueText")
+.get(verifyEmail)
 
 
 
