f4d8958c1951df915abfad913f2198d89b253473
kairayary <kaira.abreu@gmail.com>
2022-03-02 22:20:59 -0300

    se coloco banderas dinamicas

--- src/components/pages/City/CityItinerary.js
+++ src/components/pages/City/CityItinerary.js
@@ -1,37 +1,46 @@
 import React from "react";
+import { useStateValue } from "../../../StateProvider";
 import "../City/CityItinerary.css";
 import viajar from "../../Imagen/viajar.jpeg";
 import viaje1 from "../../Imagen/viaje1.jpg";
 import viaje3 from "../../Imagen/viaje3.jpg";
 
 
-function CityItinerary() {
+
+function CityItinerary(props) { 
+
+const city = props.citySelecter
     return (
         <>
             <div class="heading">
-                <div className="headingItem">
+                {city.map(data => ( 
+                    <div className="headingItem">
 
-                    <div className="heading-Items">
-                        <img src={viajar}   />
+                        <div className="heading-Items">
+                            <img src={process.env.PUBLIC_URL + `/Imagenes/Flags/${data.flag}`}/>
+
+                        </div>
+
+                        <div className="heading-Items">
+                            <img src={process.env.PUBLIC_URL + `/Imagenes/Flags/${data.flag}`}/>
+
+                        </div>
+
+                        <div className="heading-Items">
+                            <img src={process.env.PUBLIC_URL + `/Imagenes/Flags/${data.flag}`} />
+
+                        </div>
 
                     </div>
-
-                    <div className="heading-Items">
-                        <img src={viaje1}  />
-
-                    </div>
-
-                    <div className="heading-Items">
-                        <img src={viaje3}  />
-
-                    </div>
-                </div>
+                 ))}
                 <div className="haedingTitle">
                     <h1>ITINERARIES</h1>
                 </div>
 
             </div>
             <div className="headingText">
+                <h4>"There are too many adventures out there waiting to be lived"</h4>
+
                 <span>Select the options and find the perfect itineraries for you... THE ADVENTURE BEGINS!</span>
             </div>
         </>
