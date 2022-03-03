8135546def57311e893583d22c207404d74fe4e6
kairayary <kaira.abreu@gmail.com>
2022-03-02 21:58:25 -0300

    Se agrego al modelo, uniqueText y emailVerificado

---
+++ backend/models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require ("mongoose")// trae los datos de las ciudades y la conecta
+
+const usersSchema = new mongoose.Schema({
+
+     firstname:{ type: String, require: true},
+     lastname:{ type: String, require: true},
+     email:{ type: String, require: true},
+     password:{ type: String, require: true},
+     uniqueText:{ type: String, require: true},
+     emailVerificado:{type:Boolean,require:true},
+    
+})
+
+const User= mongoose.model("users", usersSchema)
+
+module.exports = User;
