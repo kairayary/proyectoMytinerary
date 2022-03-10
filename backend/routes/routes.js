ec6ffc1d5e2157914f56cbe3cefe69e0b13bf648
kairayary <kaira.abreu@gmail.com>
2022-03-10 19:58:00 -0300

    agrego ruta singout

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -2,7 +2,7 @@ const Router = require("express").Router();
 const datosController = require ("../controllers/datosControllers")// revisar esta ruta
 const{ObtenerDatosCompletos, ObtenerItinerarios}= datosController// extrae la constante obtenerDatosCompletos destructuracion
 const usersControllers =require("../controllers/usersControllers")
-const{nuevoUsuario, verifyEmail} = usersControllers
+const{nuevoUsuario, verifyEmail, accesoUsuario,cerrarSesion} = usersControllers
 const validator = require("../controllers/validator")
 
 
@@ -16,6 +16,12 @@ Router.route("/signup")
 
 Router.route("/verify/:uniqueText")
 .get(verifyEmail)
+
+Router.route("/signin")
+.post(accesoUsuario)
+
+Router.route("/signout")
+.post(cerrarSesion)
 
 
 
