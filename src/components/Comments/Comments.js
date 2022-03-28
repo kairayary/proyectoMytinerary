fe8443476b056234a1af589aa9c6b339725f269c
kairayary <kaira.abreu@gmail.com>
2022-03-28 15:12:24 -0300

    Agregue notas

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -76,7 +76,7 @@ function Comment(props) {
         <div class="accordion-item">
           <h2 class="accordion-header" id="flush-headingOne">
             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-              Accordion Item #1
+              COMMENTS
             </button>
           </h2>
 
@@ -107,97 +107,9 @@ function Comment(props) {
                 <button type="submit" className="btn btn-primary">Send</button>
               </div>
             </form>
-
-
           </div>
-
-
-
         </div>
-
-
-
-
-
-
       </div>
-
-      {/* <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-          Comments
-        </button> */}
-
-      {/* <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="staticBackdropLabel"> Comment Your Experience</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-              <form >
-                <div class="mb-3">
-                  <label for="exampleFormControlTextarea1" class="form-label"><Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-                  </label>
-                  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                </div> */}
-      {/* <textarea name="textarea" placeholder="Write us..." className="itineraryTextarea">
-             <p></p>
-          </textarea> */}
-
-      {/* </form>
-            </div>
-            <div class="modal-footer">
-              <button type="submit" class="btn btn-secondary" data-bs-dismiss="modal">Send<i className="fa fa-paper-plane"></i></button>
-              <button type="button" class="btn btn-primary">Delete</button>
-            </div>
-          </div>
-        </div>
-      </div> */}
-      {/* prueba de todo */}
-
-      {/* <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-          Comments
-        </button> */}
-
-      {/* <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-         <div className="modal-dialog"> 
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="staticBackdropLabel"> Comment Your Experience</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            {comment?.map(comm =>
-              <div className="deAquiFiltro">
-                <div className="commentUserImg">
-                  <img></img>
-                  <p>{comm.user.firstname}</p>
-                </div>
-                <div class="modal-body">
-
-                  <div className="commentText">
-
-                    <input onKeyUp={handleChange} defaultValue={comm.comment}></input>
-                  </div>
-                </div>
-                <button className="btn btn-primary mx-2" onClick={() => borrarComentario(comm._id)}>Delete</button>
-                  <button className="btn btn-primary" onClick={() => modificar(comm._id)}>Edit</button>
-
-                </div>  
-              )}
-
-                <form onSubmit={submitComment}>
-                  <textarea name="textarea" placeholder="Write us..." className="form-control" rows="3">
-                    <p></p>
-                  </textarea>
-                  <div className="btn-comentario-form">
-                    <button type="submit" className="btn btn-primary">Send</button>
-                  </div>
-                </form>
-              
-          </div>
-        </div>
-        </div>  */}
-
     </>
   );
 };
