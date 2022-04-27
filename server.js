57223814e77966055f4c6b212946893f0ed8a1c3
kairayary <kaira.abreu@gmail.com>
2022-04-27 16:45:26 -0300

    primer commit

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const passport = require("passport")
 const app = express ()
 require("./config/database")
 
+const path = require('path')
+const PORT=process.env.PORT || 4000
+const HOST=process.env.HOST || '0.0.0.0'
+
 
 //middlewares:servicios intermedios usados para realizar ciertas funciones, ejm usar respuestas en formato json
 //el express, utilizar los cors,etc
@@ -14,4 +18,13 @@ app.use(cors())
 app.use(passport.initialize())
 app.use("/api",Router)
 
-app.listen("4000",() => console.log("Servidor Inicializado en Puerto 4000"))
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+    app.get('*', (req, res)=>{
+        res.sendFile(path.join(__dirname+'/client/build/index.html'))
+
+    })
+    
+} 
+
+app.listen(PORT, HOST, ()=> console.log("Servidor Inicializado en Puerto 4000"))
